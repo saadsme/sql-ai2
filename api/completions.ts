a52ed4fb7c5bd78072ea365506b1457beefd10cd
xosaad <xosaad@users.noreply.github.com>
2023-06-06 20:54:41 +0400

    try2

--- api/completions.ts
+++ api/completions.ts
@@ -10,20 +10,25 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: 'Return ONLY SQL for ' + req.body.message,
-        },
-      ],
-    });
-    console.log(completion);
-    res.status(200).json(completion.data.choices[0].message);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
+  if (req.method === 'POST') {
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: 'Return ONLY SQL for ' + req.body.message,
+          },
+        ],
+      });
+      console.log(completion);
+      res.status(200).json(completion.data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
 }
