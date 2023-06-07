c33a30abecb64a1e3fc51529ee27bed53c777fbe
xosaad <xosaad@users.noreply.github.com>
2023-06-07 17:29:33 +0400

    try2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 export const metadata = {
   title: 'Text to SQL Tool',
   description: 'Side Project by Ahmed Saad',
-  image: 'https://ai-sql.tech/FinalSQL.png',
+  
 }
 
 export default function RootLayout({
