6b49220f10ff8f956bd7e0455fda69c86fb4ebfb
xosaad <xosaad@users.noreply.github.com>
2023-06-07 20:43:08 +0400

    OG-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import { Analytics } from '@vercel/analytics/react';
 export const metadata = {
   title: 'Text to SQL Tool',
   description: 'Side Project by Ahmed Saad',
-  
+  ogImage: {
+    url: 'public/OG_image.png', // Replace with the URL of your OG image
+    alt: 'Open Graph Image',
+  },
 }
 
 export default function RootLayout({
