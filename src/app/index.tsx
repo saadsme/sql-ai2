a9e22c29b4f1bfb8aa5e30ca62d59203c62ee641
xosaad <xosaad@users.noreply.github.com>
2023-06-07 17:35:47 +0400

    try3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Analytics />
+    
   </React.StrictMode>
 );
 
